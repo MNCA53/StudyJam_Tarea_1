9b565f6314cc04b634a47767cca869de0ece88ce
Mariana Condori <mariananca.mc@gmail.com>
2017-04-19 15:35:46 -0400

    Commit inicial

---
+++ app/src/main/java/com/example/hp/tarea/Copy.java
@@ -0,0 +1,14 @@
+package com.example.hp.tarea;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Copy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_copy);
+    }
+}
